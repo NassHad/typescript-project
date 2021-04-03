7d65daf37121eb9f2cd88390e4dc01c0fcec2af4
Nassim <nassim.haddad.pro@gmail.com>
2021-04-03 13:23:35 +0200

    Final version

---
+++ src/app.ts
@@ -0,0 +1,42 @@
+import {Payment} from './classes/Payment.js';
+import {Invoice} from "./classes/Invoice.js";
+import {HasFormatter} from "./interfaces/HasFormatter.js";
+import {ListTemplate} from "./classes/ListTemplate";
+
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+
+// inputs
+const type = document.querySelector('#type') as HTMLSelectElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
+
+// list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
+
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+
+    let values: [string, string, number];
+    values = [tofrom.value, details.value, amount.valueAsNumber];
+
+    let doc: HasFormatter;
+    if(type.value === 'invoice'){
+        doc = new Invoice(...values);
+    } else {
+        doc = new Payment(...values);
+    }
+    list.render(doc, type.value, 'end');
+})
+
+// tuples
+
+let arr = ['ryu', 25, true];
+arr[0] = false;
+arr[1] = 'ken';
+arr = [30, false, 'ken'];
+
+let tup: [string, number, boolean] = ['ryu', 25, true];
+console.log(tup);
+
